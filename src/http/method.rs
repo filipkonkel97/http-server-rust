eeb09058b139e7c50eb3da06b057afe4986712bd
Filip Konkel <filipkonkel97@gmail.com>
2026-06-25 20:01:32 +0200

    feat: parse HTTP method and query string from requests

--- src/http/method.rs
+++ src/http/method.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 pub enum Method {
     GET,
     DELETE,
@@ -9,3 +10,24 @@ pub enum Method {
     TRACE,
     PATCH,
 }
+
+impl FromStr for Method {
+    type Err = MethodError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "GET" => Ok(self::GET),
+            "DELETE" => Ok(self::DELETE),
+            "POST" => Ok(self::POST),
+            "PUT" => Ok(self::PUT),
+            "HEAD" => Ok(self::HEAD),
+            "CONNECT" => Ok(self::CONNECT),
+            "OPTIONS" => Ok(self::OPTIONS),
+            "TRACE" => Ok(self::TRACE),
+            "PATCH" => Ok(self::PATCH),
+            _ => Err(MethodError),
+        }
+    }
+}
+
+pub struct MethodError;
--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,6 @@
-use super::method::Method;
+use crate::http::method::MethodError;
+
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
@@ -22,11 +24,20 @@ impl TryFrom<&[u8]> for Request {
         let request = str::from_utf8(buf)?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
+
+        let mut query_string = None;
+
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i];
         }
 
         unimplemented!()
@@ -58,6 +69,12 @@ impl ParseError {
             Self::InvalidProtocol => "Invalid Protocol",
             Self::InvalidMethod => "Invalid Method",
         }
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
     }
 }
 
