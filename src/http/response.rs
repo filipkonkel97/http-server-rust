5cfe93aacd69c6dfeb42e73ec1a7f8b445ab4824
Filip Konkel <filipkonkel97@gmail.com>
2026-07-08 17:31:49 +0200

    Fix HTTP response body handling

--- src/http/response.rs
+++ src/http/response.rs
@@ -22,9 +22,10 @@ impl Response {
         };
         write!(
             stream,
-            "HTTP/1.1 {} {} \r\n\r\n",
+            "HTTP/1.1 {} {} \r\n\r\n{}",
             self.status_code,
             self.status_code.reason_phrase(),
+            body
         )
     }
 }
